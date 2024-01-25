7388767046ae0b9df370b45cb6862a5feaf2d919
Ronan Vieira do Carmo Juior <ronanvcjunior@gmail.com>
2024-01-25 14:24:11 -0300

    refac: Atualização do domain Alergia para o nome ser unique

--- grails-app/domain/agendavacinacao/Alergia.groovy
+++ grails-app/domain/agendavacinacao/Alergia.groovy
@@ -6,7 +6,7 @@ import grails.gorm.annotation.Entity
 class Alergia {
     String nome
     static constraints = {
-        nome nullable: false, maxSize: 60
+        nome nullable: false, maxSize: 60, unique: true
     }
 
     static mapping = {
